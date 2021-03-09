00acf96f91ee8b1089aaa7acfea54948e9d15f01
Khresterion (Jeremy) <axios.ludis@gmail.com>
2021-03-09 15:54:52 +0100

    Barres de progression skills animées

--- js/script.js
+++ js/script.js
@@ -45,3 +45,27 @@ $(".image").click(function () {
   $(".image").removeClass("active");
   $(this).addClass("active");
 });
+
+// PROGRESSBAR
+
+let delay = 500;
+$(".progress-bar").each(function (i) {
+  $(this)
+    .delay(delay * i)
+    .animate({ width: $(this).attr("aria-valuenow") + "%" }, delay);
+
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: delay,
+        easing: "swing",
+        step: function (now) {
+          $(this).text(Math.ceil(now) + "%");
+        },
+      }
+    );
+});
