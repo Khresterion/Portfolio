cebf3209cb657aad125d17bbf46f1edecb9638da
Khresterion (Jeremy) <axios.ludis@gmail.com>
2021-03-12 22:30:32 +0100

    Résolution link icones by mister D.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
 // -------------
-// BURGER
-// -------------
-$(function () {
-  $(".navbar a, footer a").on("click", function (event) {
-    event.preventDefault();
-    let hash = this.hash;
-
-    $("body,html").animate(
-      { scrollTop: $(hash).offset().top },
-      900,
-      function () {
-        window.location.hash = hash;
-      }
-    );
-  });
-});
-
-// -------------
 // TYPE SCRIPT
 // -------------
 let typed3 = new Typed(".typed", {
