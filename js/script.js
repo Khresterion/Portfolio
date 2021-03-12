f87047176acb300585d146dbddc36dce62ffa54e
Dimitri Pommerole <dpommerole@gmail.com>
2021-03-12 21:49:53 +0100

    chore: revert and comment

--- js/script.js
+++ js/script.js
@@ -3,8 +3,16 @@
 // -------------
 // $(function () {
 //   $(".navbar a, footer a").on("click", function (event) {
-//     // event.preventDefault();
-//     // window.open(event.currentTarget.href, '_blank')
+//     event.preventDefault();
+//     let hash = this.hash;
+
+//     $("body,html").animate(
+//       { scrollTop: $(hash).offset().top },
+//       900,
+//       function () {
+//         window.location.hash = hash;
+//       }
+//     );
 //   });
 // });
 
